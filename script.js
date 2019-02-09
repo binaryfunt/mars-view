694c93eb35a5a1db5609eba3c1fab51be97243bb
Ronnie Dutta <binaryfunt@users.noreply.github.com>
2019-02-09 17:39:20 +0000

    Switch to background-image
    When will I be free of using background-image?

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     var mainDiv = $("#main")[0];
 
-    $.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2019-1-1&camera=navcam&api_key=" + APIKey, function (data) {
+    $.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-05-30&camera=navcam&api_key=" + APIKey, function (data) {
         updateDisplay(data.photos[0]);
         // TODO: Handle case when no images
     });
@@ -12,8 +12,7 @@ $(document).ready(function () {
     function updateDisplay(data) {
         var element = React.createElement(
             "div",
-            { "class": "container" },
-            React.createElement("img", { src: data.img_src }),
+            { "class": "container", style: { backgroundImage: "url(" + data.img_src + ")" } },
             React.createElement(
                 "div",
                 { "class": "info" },
--- src/script.js
+++ src/script.js
@@ -4,15 +4,14 @@ $(document).ready(() => {
 
     const mainDiv = $("#main")[0];
 
-    $.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2019-1-1&camera=navcam&api_key=${APIKey}`, (data) => {
+    $.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-05-30&camera=navcam&api_key=${APIKey}`, (data) => {
         updateDisplay(data.photos[0]);
         // TODO: Handle case when no images
     });
 
     function updateDisplay(data) {
         const element = (
-            <div class="container">
-                <img src={data.img_src} />
+            <div class="container" style={{backgroundImage: "url(" + data.img_src + ")"}}>
                 <div class="info">
                     <span id="capture-date">{data.earth_date}</span>
                 </div>
