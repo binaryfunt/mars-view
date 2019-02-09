100d4da0794830c05f50f0f1f99e581e99c5730a
Ronnie Dutta <binaryfunt@users.noreply.github.com>
2019-02-09 15:49:47 +0000

    Use React
    (Without babel or JSX)

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const APIKey = 'Y5iGrr42MbNPB7CZYUkkwz9uaSLzDaRecvoOXCb9';
 
 $(document).ready(() => {
 
-    const mainDiv = $("#main");
+    const mainDiv = $("#main")[0];
 
     $.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=navcam&api_key=${APIKey}`, (data) => {
-        let img = $('<img />', {
-            src: data.photos[0].img_src
-        });
-        mainDiv.append(img);
+        updateDisplay(data.photos[0].img_src);
     });
 
+    function updateDisplay(img_src) {
+        const element = React.createElement(
+            'img',
+            {src: img_src}
+        );
+        ReactDOM.render(element, mainDiv);
+    }
 });
