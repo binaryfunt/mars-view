2f3014af3b43a59e2d9c5ed24af5c6a0471326ea
Ronnie Dutta <binaryfunt@users.noreply.github.com>
2019-02-10 00:27:40 +0000

    Implement "slide off" effect for info

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const APIKey = 'Y5iGrr42MbNPB7CZYUkkwz9uaSLzDaRecvoOXCb9';
 
 const slideAdvanceDelay = 5000;
-const slideTransitionDuration = 1;
+const transitionDurSec = 1;
+const transitionDurMilSec = transitionDurSec * 1000;
 
 function wait(delay) {
     return new Promise(resolve => setTimeout(resolve, delay));
@@ -12,7 +13,7 @@ $(document).ready(() => {
     const mainDiv = $("#main")[0];
 
     $.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-05-30&camera=navcam&api_key=${APIKey}`, (data) => {
-        updateDisplay(data.photos[0]);
+        setData(data.photos[0]);
         // TODO: Handle case when no images
     });
 
@@ -25,81 +26,15 @@ $(document).ready(() => {
             date: '',
             sol: '',
             cameraFullName: '',
-            slideTransitionDuration: slideTransitionDuration
+            transitionDurSec: transitionDurSec
         },
         mounted : function() {
-            window.setTimeout(() => {
-                updateDisplay({
-                  "id": 424905,
-                  "sol": 1000,
-                  "camera": {
-                    "id": 22,
-                    "name": "MAST",
-                    "rover_id": 5,
-                    "full_name": "Mast Camera"
-                  },
-                  "img_src": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631300503690E01_DXXX.jpg",
-                  "earth_date": "2015-05-30",
-                  "rover": {
-                    "id": 5,
-                    "name": "Curiosity",
-                    "landing_date": "2012-08-06",
-                    "launch_date": "2011-11-26",
-                    "status": "active",
-                    "max_sol": 2314,
-                    "max_date": "2019-02-08",
-                    "total_photos": 347231,
-                    "cameras": [
-                      {
-                        "name": "FHAZ",
-                        "full_name": "Front Hazard Avoidance Camera"
-                      },
-                      {
-                        "name": "NAVCAM",
-                        "full_name": "Navigation Camera"
-                      },
-                      {
-                        "name": "MAST",
-                        "full_name": "Mast Camera"
-                      },
-                      {
-                        "name": "CHEMCAM",
-                        "full_name": "Chemistry and Camera Complex"
-                      },
-                      {
-                        "name": "MAHLI",
-                        "full_name": "Mars Hand Lens Imager"
-                      },
-                      {
-                        "name": "MARDI",
-                        "full_name": "Mars Descent Imager"
-                      },
-                      {
-                        "name": "RHAZ",
-                        "full_name": "Rear Hazard Avoidance Camera"
-                      }
-                    ]
-                  }
-                });
-            }, slideAdvanceDelay);
+            window.setTimeout(advanceSlide, slideAdvanceDelay);
         }
     });
 
-    //     componentDidMount() {
-    //
-    //     }
-    //
-    //     update(data) {
-    //         slideOffBottom('#info').then(() => {
-    //             this.setState({
-    //                 data: data
-    //             });
-    //         });
-    //     }
-    // });
-
-    function updateDisplay(data) {
-        slide.imgSrc = data.img_src;
+    function setData(data) {
+        slide.imgSrc = data .img_src;
         slide.roverName = data.rover.name;
         slide.roverStatus = data.rover.status;
         slide.date = data.earth_date;
@@ -107,9 +42,27 @@ $(document).ready(() => {
         slide.cameraFullName = data.camera.full_name;
     }
 
+    function advanceSlide() {
+        slideOffBottom("#info").then(() => {
+            setData({
+              "id": 424905,
+              "sol": 1000,
+              "camera": {
+                "full_name": "Mast Camera"
+              },
+              "img_src": "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631300503690E01_DXXX.jpg",
+              "earth_date": "2015-05-30",
+              "rover": {
+                "name": "Curiosity",
+                "status": "active",
+              }
+            });
+        });
+    }
+
 
     async function slideOffBottom(jQuerySelector) {
         $(jQuerySelector).css("transform", "translateY(100%)");
-        await wait(slideTransitionDuration);
+        await wait(transitionDurMilSec);
     }
 });
